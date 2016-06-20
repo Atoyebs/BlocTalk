36cbc38871f68c728505e10cb7f98a2bdfb41960
Inioluwa Atoyebi <ini@waffleapp.co>
2016-06-20 22:16:09 +0100

    Adding project again, this time without core data

---
+++ BlocTalk/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BlocTalk
+//
+//  Created by Inioluwa Work Account on 20/06/2016.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
