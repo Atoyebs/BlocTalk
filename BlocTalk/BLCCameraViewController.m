6836233e5ea51905e848aa4adaf5b9f5595e4f97
Inioluwa Atoyebi <ini@waffleapp.co>
2016-06-25 21:44:42 +0100

    got camera working

--- BlocTalk/BLCCameraViewController.m
+++ BlocTalk/BLCCameraViewController.m
@@ -7,16 +7,25 @@
 //
 
 #import "BLCCameraViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface BLCCameraViewController ()
 
 #pragma mark UI Controls
 
-@property (weak, nonatomic) IBOutlet UIImageView *cameraPreview;
-
 @property (weak, nonatomic) IBOutlet UIButton *imagePickerButton;
 
 @property (weak, nonatomic) IBOutlet UIButton *cancelButton;
+
+@property (weak, nonatomic) IBOutlet UIView *cameraImagePreview;
+
+
+
+#pragma mark AVFoundation
+
+@property (nonatomic, strong) AVCaptureSession *cameraSession;
+@property (nonatomic, strong) AVCaptureStillImageOutput *cameraStillImageOutput;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;
 
 
 #pragma mark Constraints
@@ -30,8 +39,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.hasSetupConstraints = NO;
-    
+    [self setupImageCapture];
     
 }
 
@@ -59,9 +67,122 @@
 - (IBAction)changeCamera:(id)sender {
     
     NSLog(@"Change Camera Button Pressed");
+    
+    [self switchInUseCamera];
 }
 
 
+-(void)viewWillLayoutSubviews {
+    
+    [super viewWillLayoutSubviews];
+    
+    self.captureVideoPreviewLayer.frame = self.cameraImagePreview.bounds;
+    
+}
+
+
+- (void) setupImageCapture {
+    
+    
+    self.cameraSession = [[AVCaptureSession alloc] init];
+    self.cameraSession.sessionPreset = AVCaptureSessionPresetHigh;
+    
+    self.captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.cameraSession];
+    self.captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    self.captureVideoPreviewLayer.masksToBounds = YES;
+    [self.cameraImagePreview.layer addSublayer:self.captureVideoPreviewLayer];
+    
+    
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+       
+        dispatch_async(dispatch_get_main_queue(), ^{
+           
+            if (granted) {
+                
+                AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+                NSError *error = nil;
+                
+                AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+                
+                if (!input) {
+                    
+                    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:error.localizedDescription message:error.localizedRecoverySuggestion preferredStyle:UIAlertControllerStyleAlert];
+                    
+                    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+                    
+                    [alertController addAction:okAction];
+                    
+                    [self presentViewController:alertController animated:YES completion:nil];
+                    
+                }
+                else {
+                    
+                    [self.cameraSession addInput:input];
+                    self.cameraStillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+                    self.cameraStillImageOutput.outputSettings = @{AVVideoCodecKey: AVVideoCodecJPEG};
+                    
+                    [self.cameraSession addOutput:self.cameraStillImageOutput];
+                    
+                    [self.cameraSession startRunning];
+                    
+                }
+                
+            }
+            else {
+                
+                 UIAlertController *cameraPermisisonsAlertController = [UIAlertController alertControllerWithTitle:@"Camera Permission Denied" message:@"This app doesn't have permission to use the camera; please update your privacy settings." preferredStyle:UIAlertControllerStyleAlert];
+                
+                UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+                
+                [cameraPermisisonsAlertController addAction:okAction];
+                
+                [self presentViewController:cameraPermisisonsAlertController animated:YES completion:nil];
+                
+            }
+            
+        });
+        
+    }];
+    
+}
+
+- (void) switchInUseCamera {
+    
+    AVCaptureDeviceInput *currentCameraInput = self.cameraSession.inputs.firstObject;
+    
+    NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    
+    if (devices.count > 1) {
+        
+        NSUInteger currentIndex = [devices indexOfObject:currentCameraInput.device];
+        NSUInteger newIndex = 0;
+        
+        if (currentIndex < devices.count - 1) {
+            newIndex = currentIndex + 1;
+        }
+        
+        AVCaptureDevice *newCamera = devices[newIndex];
+        AVCaptureDeviceInput *newVideoInput = [[AVCaptureDeviceInput alloc] initWithDevice:newCamera error:nil];
+        
+        if (newVideoInput) {
+            UIView *fakeView = [self.cameraImagePreview snapshotViewAfterScreenUpdates:YES];
+            fakeView.frame = self.cameraImagePreview.frame;
+            [self.view insertSubview:fakeView aboveSubview:self.cameraImagePreview];
+            
+            [self.cameraSession beginConfiguration];
+            [self.cameraSession removeInput:currentCameraInput];
+            [self.cameraSession addInput:newVideoInput];
+            [self.cameraSession commitConfiguration];
+            
+            [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+                fakeView.alpha = 0;
+            } completion:^(BOOL finished) {
+                [fakeView removeFromSuperview];
+            }];
+        }
+    }
+    
+}
 
 /*
 #pragma mark - Navigation
