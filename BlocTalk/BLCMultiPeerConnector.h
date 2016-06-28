d5bb5d2acf30556d565c54f834eb6c622779a11a
Inioluwa Atoyebi <ini@waffleapp.co>
2016-06-28 12:47:53 +0100

    Starting work on multipeer connectivity

---
+++ BlocTalk/BLCMultiPeerConnector.h
@@ -0,0 +1,13 @@
+//
+//  BLCMultiPeerConnector.h
+//  BlocTalk
+//
+//  Created by Inioluwa Work Account on 28/06/2016.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BLCMultiPeerConnector : NSObject
+
+@end
---
+++ BlocTalk/BLCMultiPeerConnector.m
@@ -0,0 +1,13 @@
+//
+//  BLCMultiPeerConnector.m
+//  BlocTalk
+//
+//  Created by Inioluwa Work Account on 28/06/2016.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "BLCMultiPeerConnector.h"
+
+@implementation BLCMultiPeerConnector
+
+@end
